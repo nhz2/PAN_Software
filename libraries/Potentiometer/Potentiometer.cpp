8d8c7cebae7b24f84cf5b03dc5e2ad6b12fe847b
nhz2 <nhz2@cornell.edu>
2018-07-30 18:01:04 -0400

    AD5254 untested driver

--- libraries/Potentiometer/Potentiometer.cpp
+++ libraries/Potentiometer/Potentiometer.cpp
@@ -1,9 +1,74 @@
 //
 // libraries/Potentiometer/Potentiometer.cpp
 //
-// Created by Kyle Krol (kpk63@cornell.edu) on 07/30/2018
+// Created by Nathan Zimmerberg (nhz2@cornell.edu) on 07/30/2018
 //
 // Pathfinder for Autonomous Navigation
 // Space Systems Design Studio
 // Cornell University
 //
+
+
+AD5254(i2c_t3 &i2c_wire, uint8_t i2c_addr) : I2CDevice(i2c_wire, i2c_addr, 0) {
+  for(int i = 0; i < 3; i++)
+    this->rdac[i] = 0;
+}
+
+void set_rdac(uint8_t rdac0, uint8_t rdac1, uint8_t rdac2){
+  rdac[0]=rdac0;
+  rdac[1]=rdac1;
+  rdac[2]=rdac2;
+}
+uint8_t get_rdac0(){
+  return rdac[0];
+}
+uint8_t get_rdac1(){
+  return rdac[1];
+}
+uint8_t get_rdac2(){
+  return rdac[2];
+}
+
+bool read_block(){
+  i2c_pop_errors();
+
+  // Request data
+  uint8_t data[3];
+  i2c_write_byte(0x00);
+  i2c_read_bytes(data, 3);
+  if(i2c_pop_errors())
+    return false;
+  // Successful read
+  for (int i = 0; i < 3; i++) {
+    rdac[i]=data[i];
+  }
+  return true;
+}
+
+bool write_block(){
+  uint8_t data[]={
+    0x00,
+    rdac[0],
+    rdac[1],
+    rdac[2]
+  };
+  i2c_pop_errors();
+  i2c_write_bytes(data,4);
+  // Process potential configuration errors
+  if(i2c_pop_errors())
+    return false;
+  return true;
+}
+
+bool write_noblock(){
+  uint8_t data[]={
+    0x00,
+    rdac[0],
+    rdac[1],
+    rdac[2]
+  };
+  i2c_begin_transmission();
+  i2c_write(data,4);
+  i2c_send_transmission();
+  return true;
+}
--- libraries/Potentiometer/Potentiometer.hpp
+++ libraries/Potentiometer/Potentiometer.hpp
@@ -1,7 +1,7 @@
 //
 // libraries/Potentiometer/Potentiometer.hpp
 //
-// Created by Kyle Krol (kpk63@cornell.edu) on 07/30/2018
+// Created by Nathan Zimmerberg (nhz2@cornell.edu) on 07/30/2018
 //
 // Pathfinder for Autonomous Navigation
 // Space Systems Design Studio
@@ -13,11 +13,45 @@
 
 #include <I2CDevice.hpp>
 
+
+/*! Possible slave addresses */
+static const uint8_t AD5254_ADDR_0 = 0b0101100;
+static const uint8_t AD5254_ADDR_1 = 0b0101101;
+static const uint8_t AD5254_ADDR_2 = 0b0101110;
+static const uint8_t AD5254_ADDR_3 = 0b0101111;
+
 class AD5254 : public I2CDevice {
 
 public:
 
+  /*! Constructs a AD5254 on the specified wire and with the given
+   *  address.
+   default states:
+      rdac values are 0
+   */
+  AD5254(i2c_t3 &i2c_wire, uint8_t i2c_addr);
+
+  /*!Sets and returns the RDAC settings*/
+  void set_rdac(uint8_t rdac0, uint8_t rdac1, uint8_t rdac2);
+  uint8_t get_rdac0();
+  uint8_t get_rdac1();
+  uint8_t get_rdac2();
+
+  /*!if sent i2c message to read rdacs
+  updates value of rdacs*/
+  bool read_block();
+
+  /*!if sent i2c message to write rdacs*/
+  bool write_block();
+
+  /*!if added i2c message to write rdacs to i2c queue
+  adds a slave address and 4 data bytes to i2c_t3's bus.
+  uses i2c stop.*/
+  bool write_noblock();
+
 private:
+  /*! RDAC settings for 0,1,and 2*/
+  uint8_t rdac[3];
 
 };
 

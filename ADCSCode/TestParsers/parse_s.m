f5141cc878f1f12f7abc3da5433827d900d59750
kkrol27 <kyle.krol.27@gmail.com>
2018-07-31 13:36:00 -0400

    new parse file

---
+++ ADCSCode/TestParsers/parse_s.m
@@ -0,0 +1,11 @@
+function parse_s(file, first_fig)
+% This function is responsible for parsing data and generating graphs for the
+% 'a' test from ADCSCode.ino
+%   file is the name of the data file
+%   first_fig is the id for the first figure to be created
+
+  data = csvread(file);
+
+  % TODO : Actually make the graphs and stuff
+
+end

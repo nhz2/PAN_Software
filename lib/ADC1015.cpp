27b49a110e82447179d956c2c83407457b3a5ab4
kkrol27 <kyle.krol.27@gmail.com>
2018-09-02 15:19:49 -0400

    ADS1015 added

---
+++ lib/ADC1015.cpp
@@ -0,0 +1,13 @@
+//
+// ADC1015.cpp
+// PAN
+//
+// Contributors:
+//   Kyle Krol  kpk63@cornell.edu
+//
+// Pathfinder for Autonomous Navigation
+// Space Systems Design Studio
+// Cornell Univeristy
+//
+
+#include "ADS1015.hpp"

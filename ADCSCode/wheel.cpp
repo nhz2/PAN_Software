9d257718fd9b20b416fce0a3d97af328e08b73d7
nhz2 <nhz2@cornell.edu>
2018-09-02 20:54:27 -0400

    Bug fix

--- ADCSCode/wheel.cpp
+++ ADCSCode/wheel.cpp
@@ -12,18 +12,18 @@
 
 #include "wheel.hpp"
 
-wheel::WheelUnit wheels[3] = {
+wheel::WheelUnit wheel::wheels[3] = {
   {39,26,23,A14,A1,AD5254(Wire1,AD5254_ADDR_1),0},
   {39,26,23,A14,A1,AD5254(Wire1,AD5254_ADDR_1),0},
   {39,26,23,A14,A1,AD5254(Wire1,AD5254_ADDR_1),0}
 };
 
-void init() {
+void wheel::init() {
   for(int i = 0; i < 3; i++) {
-    pinMode(wheels[i].en_ccw_pin,OUTPUT);
-    pinMode(wheels[i].en_cw_pin,OUTPUT);
-    pinMode(wheels[i].set_speed_pin,OUTPUT);
-    pinMode(wheels[i].read_speed_pin, INPUT);
-    pinMode(wheels[i].read_ramp_pin, INPUT);
+    pinMode(wheel::wheels[i].en_ccw_pin,OUTPUT);
+    pinMode(wheel::wheels[i].en_cw_pin,OUTPUT);
+    pinMode(wheel::wheels[i].set_speed_pin,OUTPUT);
+    pinMode(wheel::wheels[i].read_speed_pin, INPUT);
+    pinMode(wheel::wheels[i].read_ramp_pin, INPUT);
   }
 }

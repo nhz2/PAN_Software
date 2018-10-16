8234936aa174c1af123053ac4dbf019fb906f066
kkrol27 <kyle.krol.27@gmail.com>
2018-10-15 23:10:25 -0400

    i2c device and adcs warnings

--- lib/Devices/I2CDevice.hpp
+++ lib/Devices/I2CDevice.hpp
@@ -71,25 +71,33 @@ class I2CDevice : public Device {
    *  considered to succesful. If the setup function returns false, this implies
    *  the device is not functional and is marked as such. **/
   virtual bool setup() override;
-  /** \brief The device is considered to be functional if less than
-   *         I2CDEVICE_DISABLE_AT errors have occurred in a row.
-   *  \returns True if device is working properly, false otherwise.. **/
+
   virtual bool is_functional() const override;
-  /** \brief Wipes error history variables clean. Must be called from a function
-   *         that overrides this. **/
+  /** \brief Forces this i2c device to be marked as functional.
+   *
+   *  The error history variable is reset which therefore marks the device as
+   *  functional. The device will still mark itself as not functional if it has
+   *  I2CDEVICE_DISABLE_AT i2c errors in a row. **/
   virtual void reset() override;
-  /** \brief Forces error history variables to broken state. Must be called from
-   *         a function that overrides this. **/
+  /** \brief Forces this i2c device to be marked as not functional.
+   *
+   *  The error history variable is set to I2CDEVICE_DISABLE_AT which causes the
+   *  i2c device to be seen as not functional. This can be reversed by calling
+   *  the reset function. **/
   virtual void disable() override;
-  /** \brief Sets this device's I2C timeout in milliseconds. **/
+  /** \brief Sets this device's i2c timeout in milliseconds.
+   *  \param[in] i2c_timeout New i2c timeout in milliseconds. **/
   inline void i2c_set_timeout(unsigned long i2c_timeout);
-  /** \brief  Gets the current value of i2c_timeout in milliseconds.
-   *  \return current i2c_timeout in milliseconds **/
+  /** \brief Gets the current value of i2c_timeout in milliseconds.
+   *  \return current i2c_timeout in milliseconds. **/
   inline unsigned long i2c_get_timeout() const;
-  /** \brief Returns whether or not the current data from the device should be
-   *         considered valid. For this to be true, the consecutive
-   *         communication failure variables must be zero.
-   *  \return true if data is valid and false otherwise. **/
+  /** \brief Specifies whether the most recent i2c data should be considered
+   *         valid.
+   *  \returns True if the most recent data is valid and false otherwise.
+   *
+   *  The data is considered valid if the error history variable currently has
+   *  a value of zero - i.e. the last i2c communication did not result in an
+   *  error. **/
   inline bool i2c_data_is_valid() const;
 
  protected:
@@ -98,9 +106,10 @@ class I2CDevice : public Device {
    *         recieved. i2c related errors are queried in setup.
    *  \returns true if the proper value was read and false otherwise **/
   virtual bool i2c_ping();
-  /** \brief Constructs an i2c device on the specified wire, with the given
-   *         address, and a default timeout values of 0 - i.e. a timeout never
-   *         happens. **/
+  /** \brief Constructs a new i2c device object.
+   *  \param[in] wire Reference to the Wire this i2c device is on.
+   *  \param[in] addr Address byte of this i2c device.
+   *  \param[in] timeout Timeout value fro this i2c device in milliseconds. **/
   I2CDevice(i2c_t3 &wire, uint8_t addr, unsigned long timeout = 0);
   /** \brief Returns true if an error has occurred since the last call to
    *         pop_errors and false otherwise. The recent error history variable
@@ -115,60 +124,118 @@ class I2CDevice : public Device {
    *  \returns true if an error has occurred since the last call to pop_errors
    *           and false otherwise. **/
   inline bool i2c_peek_errors() const;
-  /** \brief Sends a message over i2c to this device. The body of the message is
-   *         the array data of type T with length len. Any error will be
-   *         recorded in the recent error history variable. This is a blocking
-   *         call. **/
+  /** \brief Sends a message over i2c to this i2c device.
+   *  \tparam Type of data written to the outgoing i2c buffer.
+   *  \param[in] data Data to be written to the outgoing i2c buffer.
+   *  \param[in] len Number of elements to be written from data.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  This performs a series of blocking i2c commands to send data to this i2c
+   *  device. Any error is recorded in the recent error history variable. **/
   template <typename T>
   void i2c_transmit_data(T const *data, std::size_t len, i2c_stop s = I2C_STOP);
-  /** \brief Requests and recieves a message over i2c form this device. The
-   *         message will be placed in the array data of type T with length len.
-   *         The size of the array data will determine how much data is
-   *         requested. Any error will be recorded in the recent error history
-   *         variable. This is a blocking call. **/
+  /** \brief Requests and recieves a message over i2c from this i2c device.
+   *  \tparam T Type of data read form the incoming i2c buffer.
+   *  \param[out] data Data buffer data will be written into.
+   *  \param[in] len Length of the data buffer to be overwritten.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  This performs a series of blocking i2c commands to request data from this
+   *  i2c device, read it into the the supplied data buffer, and update the
+   *  recent error history variable. Be sure to check for data integrity with a
+   *  call to i2c_data_is_valid. **/
   template <typename T>
   void i2c_receive_data(T *data, std::size_t len, i2c_stop s = I2C_STOP);
-  /** \brief See Wire.beginTransmission in i2c_t3. **/
+  /** \brief Inlined call to Wire.beginTransmission in i2c_t3.
+   *
+   *  The i2c device's address is passed as an argument to the call to
+   *  Wire.beingTransmission. **/
   inline void i2c_begin_transmission();
-  /** \brief See Wire.endTransmission in i2c_t3. Any error will be recorded in
-   *         the recent error history variable. **/
+  /** \brief Inlined call to Wire.endTransmission from i2c_t3.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  The provided or default i2c stop flag as well as this i2c devices timeout
+   *  value are passed to Wire.endTransmission. Errors reported by
+   *  Wire.endTransmission are recorded in the recent error history variable.
+   * **/
   inline void i2c_end_transmission(i2c_stop s = I2C_STOP);
-  /** \brief See Wire.sendTransmission in i2c_t3. **/
+  /** \brief Inlined call to Wire.sendTransmission from i2c_t3.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  Only the i2c stop flag is passed to the call to Wire.sendTransmission
+   *  given it is a non-blocking call. **/
   inline void i2c_send_transmission(i2c_stop s = I2C_STOP);
-  /** \brief See Wire.requestFrom in i2c_t3. Any error will be recorded in the
-   *         recent error history variable. **/
+  /** \brief Inlined call to Wire.requestFrom in i2c_t3.
+   *  \param[in] len Length of message in bytes requested.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  The requested length, i2c stop flag, i2c device address, and i2c device
+   *  timeout are all passed to the call to Wire.requestFrom. If the proper
+   *  number of bytes is not recieved, an error is recorded in the recent error
+   *  history variable. **/
   inline void i2c_request_from(std::size_t len, i2c_stop s = I2C_STOP);
   /** \brief See Wire.sendRequest in i2c_t3. Any error will be recorded in the
    *         recent error history variable. **/
+
+  /** \brief Inlined call to Wire.sendRequest from i2c_t3.
+   *  \param[in] len Length of message in bytes requested.
+   *  \param[in] s Sets whether or not an i2c stop signal is sent at the end of
+   *               the transmission.
+   *
+   *  The requested length, i2c stop flag, and i2c device address are all passed
+   *  to the call to Wire.sendRequest. The i2c device's timeout is not because
+   *  this call is non-blocking. **/
   inline void i2c_send_request(std::size_t len, i2c_stop s = I2C_STOP);
-  /** \brief See Wire.done in i2c_t3.
+  /** \brief Inlined call to Wire.done from i2c_t3.
    *  \returns false if a non-blockign call is still running and true otherwise.
    * **/
   inline bool i2c_done() const;
-  /** \brief See Wire.finish in i2c_t3. **/
+  /** \brief Inlined call to Wire.finish from i2c_t3.
+   *
+   *  Any error is recorded in the recent error history variable. **/
   inline void i2c_finish();
   /** \brief See Wire.write in i2c_t3. Any error will be recorded in the recent
    *         error history variable. **/
+
+  /** \brief Inline call to Wire.write from i2c_t3.
+   *
+   *  Writes a single byte to the outgoing i2c buffer. Any error is recorded in
+   *  the recent error history variable. **/
   inline void i2c_write(uint8_t data);
-  /** \brief Writes data to the outgoing i2c buffer of an arbitrary type T. The
-   *         data pointer points to the head of an array of type T with length
-   *         len. **/
+  /** \brief Data type templated inlined call to Wire.write from i2c_t3.
+   *  \tparam T Type of data written to the i2c outgoing buffer.
+   *  \param[in] data Data array to be written to the i2c outgoing buffer.
+   *  \param[in] len Length of the data array.
+   *
+   *  Writes an array of arbitrary data types to the outgoing i2c buffer. Any
+   *  error is recorded in the recent error history variable. **/
   template <typename T>
   inline void i2c_write(T const *data, std::size_t len);
-  /** \brief See Wire.available in i2c_t3. **/
-  inline unsigned int i2c_available() const;
   /** \brief See Wire.readByte in i2c_t3. Any error will be recorded in the
    *         recent error history variable.
    *  \returns next uint8_t in the incoming i2c buffer. **/
+
+  /** \brief Inlined call to Wire.readByte from i2c_t3.
+   *  \returns next uint8_t in the incoming i2c buffer.
+   *
+   *  Any error is recorded in the recent error history variable. Be sure to
+   *  check for data integrity with a call to i2c_data_is_valid. **/
   inline uint8_t i2c_read();
-  /** \brief Reads data from the incoming i2c buffer in the specified array of
-   *         type T and length len. If the array cannot be filled with the
-   *         present available data, an error is recorded. **/
+  /** \brief Data type tmeplated inlined call to Wire.read from i2c_t3.
+   *  \tparam T Type of data read from the i2c incoming buffer.
+   *  \param[out] data Data array to be written to from the i2c incoming buffer.
+   *  \param[in] len Number of elements to be written over in the data array.
+   *
+   *  Fills an array of arbitrary data type with data read from the incoming
+   *  i2c buffer. Any error is recorded in the recent error history variable. Be
+   *  sure to check for data integrity with a call to i2c_data_is_valid. **/
   template <typename T>
   inline void i2c_read(T *data, std::size_t len);
-  /** \brief See Wire.peekByte in i2c_t3.
-   *  \returns next uint8_t in the incoming i2c buffer. **/
-  inline uint8_t i2c_peek();
 
  private:
   /** Wire associated with this device **/

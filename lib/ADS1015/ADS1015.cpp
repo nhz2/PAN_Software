361ecd510d05fff617ca2b952b54785721e980e3
kkrol27 <kyle.krol.27@gmail.com>
2018-11-11 16:02:49 -0500

    ADS1015 driver update

--- lib/ADS1015/ADS1015.cpp
+++ lib/ADS1015/ADS1015.cpp
@@ -20,31 +20,80 @@ void ADS1015::single_comp_test() {
 
 bool ADS1015::i2c_ping() {
   // TODO
-  return false;
+  return true;
 }
 
 /** \brief Sample rate delays in milliseconds. **/
 static unsigned long const sample_rates[] = {128,  250,  490, 920,
                                              1600, 2400, 3300};
 
-ADS1015::ADS1015(i2c_t3 &wire, ADS1015_ADDR addr, unsigned int alert_pin,
-                 unsigned long timeout)
-    : I2CDevice(wire, addr, timeout), alert_pin(alert_pin) {
-  // TODO
+ADS1015::ADS1015(i2c_t3 &wire, ADDR addr, unsigned long i2c_timeout,
+                 unsigned int alert_pin)
+    : I2CDevice(wire, addr, i2c_timeout), alert_pin(alert_pin) {
+  this->timestamp = 0ul;
+  this->alert_config_needed = true;
+  this->set_sample_rate(SR::SPS_1600);
+  this->set_gain(GAIN::PM_2_048V);
 }
 
-void ADS1015::set_sample_rate(ADS1015_SR sample_rate) {
+void ADS1015::set_sample_rate(SR sample_rate) {
   this->sample_delay = (1000 / sample_rates[sample_rate >> 5]) + 1;
   this->sample_rate = sample_rate;
 }
 
 void ADS1015::start_read(unsigned int channel) {
-  // TODO
+  // Check if the device needs to be configured
+  if (this->alert_config_needed) {
+    // Threshold configurationd data to enable alert pin
+    static uint8_t const thresh[] = {0x02, 0x7F, 0xFF, 0x03, 0x80, 0x00};
+    // Write high thresh register
+    i2c_begin_transmission();
+    i2c_write(&thresh[0], 3);
+    i2c_end_transmission(I2C_NOSTOP);
+    // Write low thresh register
+    i2c_begin_transmission();
+    i2c_write(&thresh[3], 3);
+    i2c_send_transmission(I2C_NOSTOP);
+  }
+  // Determine the configuration signal
+  uint16_t config = 0x8108 | this->sample_rate | this->gain |
+                    (((channel % 4) << 12) + 0x4000);
+  uint8_t arr[] = {0x01, (uint8_t)(config >> 0x8), (uint8_t)(config & 0xFF)};
+  // Wait for non-blocking transmission and write configuration
+  i2c_finish();
+  i2c_begin_transmission();
+  i2c_write(arr, 3);
+  i2c_end_transmission();
+  // Record conversion start time
+  this->timestamp = millis();
 }
 
 int16_t ADS1015::end_read() {
-  // TODO
-  return 0;
+  // Wait for alert pin with timeout option
+  unsigned long offset = 0;
+  if (this->timestamp + (this->sample_delay << 7) < this->timestamp)
+    offset = (this->sample_delay << 7);
+  while (digitalRead(this->alert_pin) == LOW &&
+         millis() + offset <= this->timestamp + offset + this->sample_delay)
+    ;
+  // Configure register for data read
+  i2c_begin_transmission();
+  i2c_write(0x00);
+  i2c_end_transmission(I2C_NOSTOP);
+  // Request and read in data
+  uint8_t data[2];
+  i2c_request_from(2);
+  i2c_read(data, 2);
+  // Check for errors
+  if (i2c_pop_errors()) {
+    // Failed
+    this->alert_config_needed = true;
+    return false;
+  }
+  // Success
+  int16_t x = (int16_t)((data[0] << 4) | (data[1] >> 4));
+  this->alert_config_needed = false;
+  return x;
 }
 
 int16_t ADS1015::read(unsigned int channel) {
--- lib/ADS1015/ADS1015.hpp
+++ lib/ADS1015/ADS1015.hpp
@@ -31,11 +31,14 @@ inline namespace ADS1015_V1 {
  *  https://github.com/adafruit/Adafruit_ADS1X15. **/
 class ADS1015 : public Devices::I2CDevice {
  public:
+  /** \enum ADDR
+   *  \brief Possible i2c addresses for the \c ADS1015. **/
+  enum ADDR { GND = 0x48, VDD = 0x49, SDA = 0x4A, SCL = 0x4B };
   /** \enum SR
    *  \brief Enumerates the sample rate settings for the \c ADS1015.
    *
    *  \c SPS_100 would stand for 100 samples per second. The default samples per
-   *  second value for the \c ADS1015 is 1600. **/
+   *  second value for the \c ADS1015 is \c SPS_1600. **/
   enum SR : uint16_t {
     /** \brief 128 samples per second. **/
     SPS_128 = 0x0000,
@@ -56,16 +59,77 @@ class ADS1015 : public Devices::I2CDevice {
    *  \brief Enumerates the gain settings for the \c ADS1015.
    *
    *  The default gain value for the \c ADS1015 is \c PM_2_048V. **/
-  enum GAIN {
+  enum GAIN {  // TODO
     PM_6_1444 = 0x0000,
     PM_4_096V = 0x0200,
-    PM_2_048V = 0x0400,  // Default
+    PM_2_048V = 0x0400,
     PM_1_024V = 0x0600,
     PM_0_512V = 0x0800,
     PM_0_256V = 0x0A00
   };
+  /** \brief Outputs the result of an analog read of all four channels of the
+   *         \c ADS1015.
+   *
+   *  Requires that \c Serial has been initialized. **/
+  virtual void single_comp_test();
+  /** **/  // TODO
+  virtual bool i2c_ping();
+  /** \brief Creates an \c ADS1015 object communicating on the specified i2c bus
+   *         with the given alert gpio and timeout value.
+   *  \param[in] i2c_wire i2c bus used to communicate with the device.
+   *  \param[in] i2c_addr i2c address of this device.
+   *  \param[in] i2c_timeout Sets the this devices i2c timeout in milliseconds.
+   *  \param[in] alert_pin GPIO pin connected to this device's alert output.
+   *
+   *  It is assumed that the specified wire was already initialized and the pin
+   *  mode of \c alert_pin was previously set to \INPUT. **/
+  ADS1015(i2c_t3 &i2c_wire, ADDR i2c_addr, unsigned long timeout,
+          unsigned int alert_pin);
+  /** \brief Sets the gain of the \c ADS1015. **/
+  inline void set_gain(GAIN gain) { this->gain = gain; }
+  /** \brief Returns the current gain setting of the \c ADS1015.
+   *  \returns Current gain setting. **/
+  inline GAIN get_gain() const { return this->gain; }
+  /** \brief Sets the sample rate of the \c ADS1015. **/
+  void set_sample_rate(SR sample_rate);
+  /** \brief Returns the current sample rate of the \c ADS1015.
+   *  \returns Current sample rate. **/
+  inline SR get_sample_rate() const { return this->sample_rate; }
+  /** \brief Starts an analog conversion on the specified channel.
+   *  \param[in] channel Channel on which the analog read is started.
+   *
+   *  The result of conversion is obtained by making a call to \c end_read at
+   *  some unspecified time later on. **/
+  void start_read(unsigned int channel);
+  /** \brief Obtains the result of an analog conversion. \c start_read must have
+   *         been called.
+   *  \returns The result of the analog conversion.
+   *
+   *  Note, that to ensure to data's integrity a call to \c i2c_pop_errors
+   *  should be made. **/
+  int16_t end_read();
+  /** \brief Performs an analog converison and returns the result.
+   *  \param[in] channel Channel on which the analog read is started.
+   *  \returns The result of the analog conversion.
+   *
+   *  Essentially, this function is equivalent to making a call to \c start_read
+   *  and then \c end_read. Data integrity needs to be ensured by making a call
+   *  to \c i2c_pop_errors. **/
+  int16_t read(unsigned int channel);
 
  private:
+  /** \brief \c ADS1015 alert pin number. */
+  unsigned int const alert_pin;
+  /** \brief Previous conversion start timestamp. */
+  unsigned long timestamp;
+  /** \brief Current sample timeout delay in milliseconds. */
+  unsigned int sample_delay;
+  /** \brief Signals the \c ADS1015 needs to be configured prior to reading. */
+  bool alert_config_needed;
+  /** \brief Current sample rate setting. */
+  SR sample_rate;
+  /** \brief Current gain setting. */
+  GAIN gain;
 };
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 }  // namespace ADS1015_V1
